2bc97448bc1a18b6292bf1ec3a59d16d1e7fe7ec
toshi/pono000 <toshi.pono.h@gmail.com>
2020-06-30 21:47:31 +0900

    高額商品のときに表示を追加
    高額商品v-if

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -5,6 +5,7 @@
       <div class="item" :class="{over500: item.price >= 500}">
         <div class="name">名前： {{ item.name }}</div>
         <div class="price">{{ item.price }}円</div>
+        <div v-if="item.price >= 10000">高額商品</div>
       </div>
     </div>
     <div>
