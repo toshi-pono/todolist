b15957d0ce2daf4f84a28c9c5de068e71b44fdc8
toshi/pono000 <toshi.pono.h@gmail.com>
2020-07-03 16:20:46 +0900

    機能追加：要素の削除・全部表示機能
    いろいろ追加した

--- src/components/taskList.vue
+++ src/components/taskList.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
     <div>TaskList</div>
+    <button class="changestate" @click="changeState">{{ nowstate }}</button>
     <div v-for="task in tasks" :key="task.name">
-      <div class="task" v-if="!task.isFinish">
-        <button class="Fincheck" @click="finishTask(task)">完了</button>
+      <div class="task" v-if="isall && task.isExist">
+        <button
+          class="Fincheck"
+          v-if="!task.isFinish"
+          @click="finishTask(task)"
+        >
+          完
+        </button>
         <div class="name">{{ task.name }}</div>
         <div class="date">{{ task.date }}まで</div>
+        <button class="deleteTask" @click="deleteTask(task)">×</button>
+      </div>
+      <div class="task" v-if="!task.isFinish && !isall && task.isExist">
+        <button class="Fincheck" @click="finishTask(task)">完</button>
+        <div class="name">{{ task.name }}</div>
+        <div class="date">{{ task.date }}まで</div>
+        <button class="deleteTask" @click="deleteTask(task)">×</button>
       </div>
     </div>
     <div>
@@ -27,9 +41,13 @@ export default {
   name: "taskList",
   data() {
     return {
-      tasks: [{ name: "やること１", date: "7/1", isFinish: false }],
+      tasks: [
+        { name: "やること１", date: "7/1", isFinish: false, isExist: true }
+      ],
       newTodoName: "",
-      newTodoDate: ""
+      newTodoDate: "",
+      isall: false,
+      nowstate: "現在：すべて表示"
     };
   },
   methods: {
@@ -37,12 +55,44 @@ export default {
       this.tasks.push({
         name: this.newTodoName,
         date: this.newTodoDate,
-        isFinish: false
+        isFinish: false,
+        isExist: true
       });
     },
     finishTask(task) {
       task.isFinish = true;
+    },
+    changeState() {
+      if (!this.isall) {
+        this.nowstate = "現在：未完のみ表示";
+      } else {
+        this.nowstate = "現在：すべて表示";
+      }
+      this.isall = !this.isall;
+    },
+    deleteTask(task) {
+      task.isExist = false;
     }
   }
 };
 </script>
+
+<style>
+.task {
+  display: flex;
+  align-items: center;
+  margin: 10px;
+  border-bottom: gray 1px solid;
+}
+.Fincheck {
+  width: 2em;
+  height: 2em;
+  margin-right: 10px;
+}
+.name {
+  margin: 10px;
+}
+.date {
+  margin: 10px;
+}
+</style>
